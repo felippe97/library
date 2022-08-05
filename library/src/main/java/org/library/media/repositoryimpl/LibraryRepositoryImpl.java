e89016027048104acf43d4b1a077213f98c9f4c3
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-08-05 14:36:29 +0200

    Update LibraryRepositoryImpl.java

--- library/src/main/java/org/library/media/repositoryimpl/LibraryRepositoryImpl.java
+++ library/src/main/java/org/library/media/repositoryimpl/LibraryRepositoryImpl.java
@@ -1,11 +1,15 @@
 package org.library.media.repositoryimpl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.library.media.domain.Library;
 import org.library.media.domain.LibraryProperties;
+import org.library.media.domain.Media;
+import org.library.media.domain.MediaProperties;
 import org.sculptor.framework.accessapi.ConditionalCriteria;
 import org.sculptor.framework.accessapi.ConditionalCriteriaBuilder;
 import org.sculptor.framework.accessapi.FindByCriteriaAccess;
@@ -21,8 +25,11 @@ public class LibraryRepositoryImpl extends LibraryRepositoryBase {
 	}
 
 	public Library findLibraryByName(String name) {
-		
-		 findByCondition(null);
+		List<ConditionalCriteria> criteria = ConditionalCriteriaBuilder.criteriaFor(Library.class)
+				.withProperty(LibraryProperties.name()).eq(name).build();
 
-}
+		List<Library> result = findByCondition(criteria);
+		return result.size() > 0 ? result.get(0) : null;
+
+	}
 }
